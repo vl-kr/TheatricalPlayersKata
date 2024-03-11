f272e4a0704d07e8d0984d5444600bf33c3ef2c0
Vlado Kravčík <29161621k@gmail.com>
2024-03-11 16:22:06 +0100

    Refactor statement title and footer

--- src/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ src/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -6,14 +6,25 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class StatementPrinter
     {
+        public string GetTitle(Invoice invoice)
+        {
+            return string.Format("Statement for {0}\n", invoice.Customer);
+        }
+
+        public string GetInvoiceFooter(CultureInfo cultureInfo, int totalAmount, int volumeCredits)
+        {
+            return String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100))
+                + String.Format("You earned {0} credits\n", volumeCredits);
+        }
+
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
             var totalAmount = 0;
             var volumeCredits = 0;
-            var result = string.Format("Statement for {0}\n", invoice.Customer);
+            var result = GetTitle(invoice);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
-            foreach(var perf in invoice.Performances) 
+            foreach(var perf in invoice.Performances)
             {
                 var play = plays[perf.PlayID];
                 var thisAmount = 0;
@@ -44,8 +55,7 @@ namespace TheatricalPlayersRefactoringKata
                 result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
             }
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-            result += String.Format("You earned {0} credits\n", volumeCredits);
+            result += GetInvoiceFooter(cultureInfo, totalAmount, volumeCredits);
             return result;
         }
     }
